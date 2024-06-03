26c16af5da840aadf9b0f5d8f1bdd455d8ef6990
Daniel R <nzdanron@gmail.com>
2024-06-03 14:36:53 +1200

    feat: improved searchbox

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,22 @@
+import "./Search.css";
+
 const Search = ({ search, setSearch }) => {
   const handleChange = (e) => setSearch(e.target.value);
 
-  return <input type="text" value={search} onChange={handleChange} />;
+  return (
+    <div id="search-box">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="22px"
+        viewBox="0 -960 960 960"
+        width="22px"
+        fill="#4c4f69"
+      >
+        <path d="M784-120 532-372q-30 22-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
+      </svg>
+      <input type="text" value={search} onChange={handleChange} />
+    </div>
+  );
 };
 
 export default Search;
